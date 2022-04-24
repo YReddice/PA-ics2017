8823c2d75d8ec1039b7abfa09a12a6df771df1b3
tracer-ics2017 <tracer@njuics.org>
2022-04-24 12:07:35 +0800

    >  compile 1911506 yzb Linux PA 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:07:35 up  1:04,  1 user,  load average: 1.07, 0.61, 0.34 68bef1f7df594c138f0e5f89dc613f2ded2b3491

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -6,3 +6,13 @@ make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+// instr implements in pa2.part1
+make_EHelper(push);
+make_EHelper(pop);
+make_EHelper(call);
+make_EHelper(sub);
+make_EHelper(xor);
+make_EHelper(ret);
+
+
